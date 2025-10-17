c7a9fc5811336ea8bad0fc7b0a839a555eb3214c
mems250005006-hash <mems250005006@iiti.ac.in>
2025-10-17 16:47:09 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,1 +1,29 @@
 
+#include<iostream>
+using namespace std;
+
+struct SalesRecord {
+    string month;
+    float amount;
+};
+
+int main() {
+    SalesRecord sales[12];
+    int maxIndex = 0, minIndex = 0;
+    
+    for(int i = 0; i < 12; i++) {
+        cin >> sales[i].month >> sales[i].amount;
+    }
+    
+    for(int i = 1; i < 12; i++) {
+        if(sales[i].amount > sales[maxIndex].amount) {
+            maxIndex = i;
+        }
+        if(sales[i].amount < sales[minIndex].amount) {
+            minIndex = i;
+        }
+    }
+    
+    cout << "Max: " << sales[maxIndex].month << " Min: " << sales[minIndex].month << endl;
+    return 0;
+}
