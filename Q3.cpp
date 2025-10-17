e4acade67bf646d225fe0c61c1bb8862576f2624
mems250005006-hash <mems250005006@iiti.ac.in>
2025-10-17 16:46:39 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+
+struct Point {
+    float x, y;
+};
+
+int main() {
+    Point points[7];
+    int count = 0;
+    
+    for(int i = 0; i < 7; i++) {
+        cin >> points[i].x >> points[i].y;
+    }
+    
+    for(int i = 0; i < 7; i++) {
+        if(points[i].x > 0 && points[i].y > 0) {
+            count++;
+        }
+    }
+    
+    cout << "Points in first quadrant: " << count << endl;
+    return 0;
+}
