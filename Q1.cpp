558109352c5883fa4a63e8c60d656be3871440a2
mems250005006-hash <mems250005006@iiti.ac.in>
2025-10-17 16:11:45 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+struct Item{
+    int id;
+    float cost;
+};
+int main(){
+    Item items[6];
+    
+    items[0].id = 1;
+    items[0].cost = 45.5;
+    items[1].id = 2;
+    items[1].cost = 60.0;
+    items[2].id = 3;
+    items[2].cost=30.0;
+    items[3].id= 4;
+    items[3].cost = 75.5;
+    
+    for(int i=0; i<6; i++){
+        if (items[i].cost > 50.00){
+            cout <<" ID:" << items[i].id<< "cost:" <<items[i].cost<<endl;
+            
+        }
+}
+
+    return 0;
+}
